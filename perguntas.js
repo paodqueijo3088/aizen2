cdb6bc09a33395f6f87394cf632746c5d2dbb857
paodqueijo3088 <maycon.gama.alves@escola.pr.gov.br>
2025-06-04 10:24:42 -0300

    1609

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'mecanica',
+    'o que é um motor V8?',
+    'Um motor V8 é um tipo de motor de combustão interna com oito cilindros dispostos em duas bancadas de quatro cilindros cada, formando um "V" quando vistos de frente. Esses motores são conhecidos por produzirem alta potência e torque, o que os torna populares em carros esportivos, de luxo, SUVs grandes e picapes.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'mecanica',
+    'o que é um motor V12?',
+    'Um motor V12 é um tipo de motor de combustão interna que possui doze cilindros dispostos em duas bancadas de seis cilindros cada, formando um ângulo em "V" em torno de um virabrequim comum.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'mecanica',
+    'paraque serve a manuntenção do carro?',
+    ' manutenção do carro serve para garantir o funcionamento seguro, eficiente e duradouro do veículo.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'mecanica',
+    'Qual a principal diferença entre um motor a gasolina e um a diesel?',
+    'A diferença essencial entre um motor a gasolina e um motor a diesel está na ignição do combustível. Em motores a gasolina, uma vela de ignição cria uma faísca para inflamar a mistura ar-combustível. Já em motores a diesel, o ar é altamente comprimido, aquecendo o suficiente para que o diesel, quando injetado, se auto-inflame por compressão, sem a necessidade de uma vela.'
 )
